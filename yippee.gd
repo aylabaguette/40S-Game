4ed095ed07147e3d9290fa89efe13b79ce59a351
aylabaguette <114880593+aylabaguette@users.noreply.github.com>
2024-12-05 10:52:18 -0600

    please work

---
+++ yippee.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("test test test!")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
